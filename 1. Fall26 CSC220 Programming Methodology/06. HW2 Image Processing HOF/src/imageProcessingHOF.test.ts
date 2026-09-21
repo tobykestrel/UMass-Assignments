f3b1dcf347fc2f601df2f7262608ab12affc8532
tobykestrel <tobykestrel@gmail.com>
2026-09-21 02:01:38 -0400

    CSC220 HW2 progress

--- 1. Fall26 CSC220 Programming Methodology/06. HW2 Image Processing HOF/src/imageProcessingHOF.test.ts
+++ 1. Fall26 CSC220 Programming Methodology/06. HW2 Image Processing HOF/src/imageProcessingHOF.test.ts
@@ -7,6 +7,7 @@ import {
   isGrayish,
   makeGrayish,
   pixelBlur,
+  pxBlurAvg,
   imageBlur
 } from "./imageProcessingHOF.js";
 
@@ -15,25 +16,100 @@ function expectColorToBeCloseTo(actual: Color, expected: Color, error = 1) {
   [0, 1, 2].forEach(i => expect(Math.abs(actual[i] - expected[i])).toBeLessThanOrEqual(error));
 }
 
-describe("imageMapCoord", () => {
-  function identity(img: Image, x: number, y: number) {
-    return img.getPixel(x, y);
-  }
+function identity(img: Image, x: number, y: number) {
+  return img.getPixel(x, y);
+}
 
-  it("should return a different image", () => {
+function identityP(c: Color) {
+  return [c[0], c[1], c[2]];
+}
+
+function createGridImage(
+  w = 10, 
+  h = 10, 
+  c1: Color = COLORS.WHITE, 
+  c2: Color = COLORS.BLACK): 
+Image {
+  const checkerPatternedImg = Image.create(w, h, c1);
+  for (let i = 0; i < w; i += 2) for (let j = 0; j < h; j += 2) {
+    checkerPatternedImg.setPixel(i, j, c2);
+  } 
+  return checkerPatternedImg;
+}
+
+describe("imageMapCoord", () => {
+
+  it("should return a different image in memory", () => {
     const input = Image.create(10, 10, COLORS.WHITE);
     const output = imageMapCoord(input, identity);
     assert(input !== output);
+  });
+
+  it("should apply the function to every pixel", () => {
+    const input = Image.create(3, 3, COLORS.BLACK);
+    const output = imageMapCoord(input, (img, x, y) => [x,y,x+y]);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        expectColorToBeCloseTo(output.getPixel(i,j), [i, j, i+j]);
+      }
+    }
   });
 
   // More tests for imageMapCoord go here.
 });
 
 describe("imageMapIf", () => {
+
+  it("should return a different image in memory", () => {
+    const input = Image.create(10, 10, COLORS.WHITE);
+    const output = imageMapIf(input, ()=>true, identityP);
+    assert(input !== output);
+  });
+
+  it("should change pixels where the condition is true", () => {
+    const input = createGridImage(3,3);
+    const output = imageMapIf(
+      input,
+      (img, x, y) => img.getPixel(x,y) == COLORS.BLACK,
+      p => COLORS.RED
+    );
+    for (let i = 0; i < 3; i+=2) {
+      for (let j = 0; j < 3; j+=2) {
+        expectColorToBeCloseTo(output.getPixel(i,j), COLORS.RED);
+      }
+    }
+  });
+
   // More tests for imageMapIf go here
 });
 
 describe("mapWindow", () => {
+
+  it("should return a different image in memory", () => {
+    const input = Image.create(10, 10, COLORS.WHITE);
+    const output = mapWindow(input, [0,1],[0,1], identityP);
+    assert(input !== output);
+  });
+
+  it("should transform everything in the inclusive window", () => {
+    const input = Image.create(4, 4, [10, 20, 30]);
+
+    const output = mapWindow(
+      input,
+      [1, 2],
+      [1, 2],
+      p => [p[0] + 10, p[1] + 10, p[2] + 10]
+    );
+
+    expectColorToBeCloseTo(output.getPixel(1, 1), [20, 30, 40]);
+    expectColorToBeCloseTo(output.getPixel(2, 1), [20, 30, 40]);
+    expectColorToBeCloseTo(output.getPixel(1, 2), [20, 30, 40]);
+    expectColorToBeCloseTo(output.getPixel(2, 2), [20, 30, 40]);
+
+    expectColorToBeCloseTo(output.getPixel(0, 0), [10, 20, 30]);
+    expectColorToBeCloseTo(output.getPixel(3, 3), [10, 20, 30]);
+  });
+
   // More tests for mapWindow go here
 });
 
@@ -42,6 +118,13 @@ describe("isGrayish", () => {
 });
 
 describe("makeGrayish", () => {
+
+  it("should return a different image in memory", () => {
+    const input = Image.create(10, 10, COLORS.WHITE);
+    const output = makeGrayish(input);
+    assert(input !== output);
+  });
+  
   // More tests for makeGrayish go here
 });
 
@@ -49,6 +132,17 @@ describe("pixelBlur", () => {
   // Tests for pixelBlur go here
 });
 
+describe("pxBlurAvg", () => {
+  // Tests for pxBlurAvg go here
+});
+
 describe("imageBlur", () => {
+
+  it("should return a different image in memory", () => {
+    const input = Image.create(10, 10, COLORS.WHITE);
+    const output = imageBlur(input);
+    assert(input !== output);
+  });
+
   // Tests for imageBlur go here
 });
--- 1. Fall26 CSC220 Programming Methodology/06. HW2 Image Processing HOF/src/imageProcessingHOF.ts
+++ 1. Fall26 CSC220 Programming Methodology/06. HW2 Image Processing HOF/src/imageProcessingHOF.ts
@@ -2,7 +2,13 @@ import type { Image, Color } from "../include/image.js";
 
 export function imageMapCoord(img: Image, func: (img: Image, x: number, y: number) => Color): Image {
   // TODO
-  return img.copy(); // Dummy code to make the autograder work, replace as necessary.
+  const newImage = img.copy();
+  for (let i = 0; i < img.width; i++) {
+    for (let j = 0; j < img.height; j++) {
+      newImage.setPixel(i,j, func(img, i, j));
+    }
+  }
+  return newImage;
 }
 
 export function imageMapIf(
@@ -11,7 +17,11 @@ export function imageMapIf(
   func: (p: Color) => Color
 ): Image {
   // TODO
-  return img.copy(); // Dummy code to make the autograder work, replace as necessary.
+  return imageMapCoord(img, (img, x, y) => {
+    const p = img.getPixel(x,y);
+    if (cond(img, x, y)) { return func(p); }
+    else return p;
+  });
 }
 
 export function mapWindow(
@@ -21,25 +31,63 @@ export function mapWindow(
   func: (p: Color) => Color
 ): Image {
   // TODO
-  return img.copy(); // Dummy code to make the autograder work, replace as necessary.
+  return imageMapIf(
+    img, 
+    (img, x, y) => (
+      x <= xInterval[1] && 
+      x >= xInterval[0] && 
+      y <= yInterval[1] && 
+      y >= yInterval[0]), 
+    func
+  );
 }
 
 export function isGrayish(p: Color): boolean {
   // TODO
-  return true; // Dummy code to make the autograder work, replace as necessary.
+  const maxChannelValue = Math.max(p[0], p[1], p[2]);
+  const minChannelValue = Math.min(p[0], p[1], p[2]);
+  return ((maxChannelValue-minChannelValue) <= 85);
 }
 
 export function makeGrayish(img: Image): Image {
   // TODO
-  return img.copy(); // Dummy code to make the autograder work, replace as necessary.
+  return imageMapIf(
+    img, 
+    (img, x, y) => !isGrayish(img.getPixel(x,y)), 
+    (p) => {
+      const avgChannelValue = Math.trunc((p[0] + p[1] + p[2])/3);
+      return [avgChannelValue, avgChannelValue, avgChannelValue];
+    }
+  );
 }
 
 export function pixelBlur(img: Image, x: number, y: number): Color {
   // TODO
-  return [0, 0, 0]; // Dummy code to make the autograder work, replace as necessary.
+  return [
+    pxBlurAvg(img,x,y,0), 
+    pxBlurAvg(img,x,y,1), 
+    pxBlurAvg(img,x,y,2)
+  ];
+}
+
+export function pxBlurAvg(img: Image, x: number, y: number, chnl: number): number {
+  let pixelBlurTotal = 0;
+  let pixelsCount = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (x+i < img.width && x+i >= 0 && y+j < img.height && y+j >= 0) {
+        pixelBlurTotal += img.getPixel(x+i,y+j)[chnl];
+        pixelsCount++;
+      }
+    }
+  }
+  return Math.trunc((pixelBlurTotal/pixelsCount));
 }
 
 export function imageBlur(img: Image): Image {
   // TODO
-  return img.copy(); // Dummy code to make the autograder work, replace as necessary.
+  return imageMapCoord(
+    img, 
+    (img, x, y) => pixelBlur(img, x, y)
+  );
 }
